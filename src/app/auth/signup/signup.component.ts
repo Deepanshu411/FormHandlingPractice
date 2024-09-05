24250a4adec9fd6d96096abfe7f8060df1910cc8
Deepanshu Dixit <deepanshu6255@gmail.com>
2024-09-05 20:18:46 +0530

    Starting sign-up form with the reactive form approach

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { debounce, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(8)],
+    }),
+  });
+
+  get isValidEmail() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  };
+
+  get isValidPassword() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  };
+
+  ngOnInit() {
+    const savedItem = window.localStorage.getItem('saved-signup-form')
+    
+    if (savedItem) {
+      const loadedFromData = JSON.parse(savedItem);
+      this.form.controls.email.setValue(
+        loadedFromData.email
+      );
+    }
+
+    const subscription = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: (val) => {
+        window.localStorage.setItem(
+          'saved-signup-form',
+          JSON.stringify({ email: val.email })
+        );
+      },
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  };
+
+  onSubmit() {
+    if (this.form.invalid) {
+      return
+    }
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+    console.log(enteredEmail, enteredPassword);
+    this.form.reset();
+  }
+}
