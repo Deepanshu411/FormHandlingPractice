1210684d4b2b187fb2b568b4a2fb1377204f507a
Deepanshu Dixit <deepanshu6255@gmail.com>
2024-09-05 17:11:07 +0530

    Saving the typed value in localStorage and prepopulating the form on reload

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +16,42 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const savedItem = window.localStorage.getItem('saved-login-form');
+      
+      if (savedItem) {
+        const loadedFromData = JSON.parse(savedItem);
+        const savedEmail = loadedFromData.email;
+
+        setTimeout(() => {
+          this.form().controls['email'].setValue(savedEmail)
+        }, 1);
+      }
+
+      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
+        next: (val) =>
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: val.email })
+          ),
+      });
+
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
+
   onSubmit(formData: NgForm) {
     if (formData.form.invalid) {
-      return
+      return;
     }
     console.log(formData);
     const enteredEmail = formData.form.value.email;
     const enteredPassword = formData.form.value.password;
-    console.log(enteredEmail, enteredPassword)
+    console.log(enteredEmail, enteredPassword);
 
     formData.form.reset();
   }
